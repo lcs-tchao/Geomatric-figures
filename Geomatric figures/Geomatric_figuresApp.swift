affa662e11469ee523b90a4733b538d944369b9d
Thomas Chao <Tchao@lcs.on.ca>
2024-10-03 08:57:16 -0400

    Initial Commit

---
+++ Geomatric figures/Geomatric_figuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geomatric_figuresApp.swift
+//  Geomatric figures
+//
+//  Created by Yuk Yeung Chao on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Geomatric_figuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
